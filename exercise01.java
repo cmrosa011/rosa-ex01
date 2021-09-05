bad8e6902307ef7d54c52348d19e57bbda3574ad
cmrosa011 <90128475+cmrosa011@users.noreply.github.com>
2021-09-05 04:36:00 -0400

    Update exercise01.java

--- exercise01.java
+++ exercise01.java
@@ -1,3 +1,8 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Christian Rosa
+ */
+
 public class exercise01 {
     public static void main(String[] args) {
         var scanner = new java.util.Scanner(System.in);
